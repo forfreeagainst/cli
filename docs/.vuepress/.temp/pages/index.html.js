a149b9931d3b1c16ed80934d000d69826d80c4a4
尘少 <1982740368@qq.com>
2024-10-16 09:11:09 +0800

    docs: 优化文档内容

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,3 +1,3 @@
 import comp from "D:/soul-ocean/vuePressProject/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1728994863000,\"contributors\":[{\"name\":\"尘少\",\"email\":\"1982740368@qq.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
