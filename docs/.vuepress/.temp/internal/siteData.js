276822e5e0aaa4eca65983d9be57c6ad262c93d7
尘少 <1982740368@qq.com>
2024-10-16 09:40:36 +0800

    docs: 修改文档

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/cli/\",\"lang\":\"zh-CN\",\"title\":\"Hello VuePress\",\"description\":\"Just playing around\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/images/logo.png\"}]],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
+export const siteData = JSON.parse("{\"base\":\"/cli/\",\"lang\":\"zh-CN\",\"title\":\"Hello VuePress\",\"description\":\"Just playing around\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/images/logo.png\"}]],\"locales\":{\"/\":{\"lang\":\"zh-CN\",\"title\":\"vp-dev\",\"description\":\"vuePress\"}}}")
